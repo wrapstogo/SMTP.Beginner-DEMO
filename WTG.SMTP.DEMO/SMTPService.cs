4dbae12fa8348015b57b4e4aecce1cfb7c7f4b1b
Mads Pedersen <farnholdt@gmail.com>
2014-03-10 13:02:27 +0100

    mp:div

---
+++ WTG.SMTP.DEMO/SMTPService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using WTG.SMTP;
+
+namespace WTG.SMTP.DEMO
+{
+    public partial class SMTPService : ServiceBase
+    {
+        private SmtpServer SmtpServer;
+        
+        public SMTPService()
+        {
+            InitializeComponent();
+            this.ServiceName = "SMTPService";
+        }
+
+        private void CreateSMTP()
+        {
+            SmtpServer = new SmtpServer("localhost.com", IPAddress.Parse("127.0.0.1"));
+            SmtpServer.MailReceived += MailReceived;
+            SmtpServer.StartListener(25);
+        }
+
+        private void MailReceived(object sender, MailReceivedEventArgs e)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("ToAddress: " + e.Mail.To.Address);
+            sb.AppendLine("FromAddress: " + e.Mail.From.Address);
+            sb.AppendLine("ReceivedTime: " + DateTime.Now.ToString());
+            sb.AppendLine("Data ----------------------------");
+            sb.Append(e.Mail.RawMailData);
+            File.WriteAllText("C:\\SMTP\\Mails\\" + Guid.NewGuid().ToString() + ".txt" , sb.ToString()); 
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            CreateSMTP();
+        }
+
+        protected override void OnStop()
+        {
+            SmtpServer.Stop();
+        }
+
+
+    }
+}
---
+++ WTG.SMTP.DEMO/SMTPServiceInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WTG.SMTP.DEMO
+{
+    [RunInstaller(true)]
+    public partial class SMTPServiceInstaller : System.Configuration.Install.Installer
+    {
+        public SMTPServiceInstaller()
+        {
+            InitializeComponent();
+        }
+    }
+}
